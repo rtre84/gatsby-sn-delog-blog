c233b507f43672ab0fabf11358a7e89809e1256c
rtre84 <rtre84@users.noreply.github.com>
2022-01-03 17:37:40 -0500

    Pushing out changes made to footer.

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,8 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>&copy; {new Date().getFullYear()} ServingNiches.Org &bull; Crafted with
+            <span role="img" aria-label="love"> ❤️ </span> &nbsp;by <a href="https://servingniches.org"> Serving Niches LLC.</a></p>
       </footer>
     </div>
   )

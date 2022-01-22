e8b3f643616a36636d712ea5a98933e22d7a0c38
rtre84 <rtre84@users.noreply.github.com>
2022-01-21 22:02:44 -0500

    Changed form names.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -73,19 +73,19 @@ class ContactPage extends React.Component {
                             </div>
                             <div>
                                 <label htmlFor="w3lName">Name</label>
-                                <input type="text" name="w3lName" id="w3lName"/>
+                                <input type="text" name="name" id="w3lName"/>
                             </div>
                             <div>
                                 <label htmlFor="w3lSender">Email</label>
-                                <input type="email" name="w3lSender" id="w3lSender"/>
+                                <input type="email" name="sender" id="w3lSender"/>
                             </div>
                             <div>
                                 <label htmlFor="w3lSubject">Subject</label>
-                                <input type="text" name="w3lSubject" id="w3lSubject"/>
+                                <input type="text" name="subject" id="w3lSubject"/>
                             </div>
                             <div>
                                 <label htmlFor="w3lMessage">Message</label>
-                                <textarea name="w3lMessage" id="w3lMessage"></textarea>
+                                <textarea name="message" id="w3lMessage"></textarea>
                             </div>
                             <div style={{display: "flex", justifyContent: "flex-end"}}>
                                 <input type="submit" className="button -primary" style={{marginRight: 0}}/>
